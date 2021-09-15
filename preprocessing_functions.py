0a607ee9e2d243b249384f3d3974546b6dfdcb27
Pipe Galera <pipegalera@gmail.com>
2021-09-15 18:54:01 +0200

    :arrow_up: Updated all the data needed for the modeling

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -22,30 +22,5 @@ def create_unit_indicator(df):
 
     return df
 
-def create_summed_PER_by_units(df):
-    # Calculate the summed PER of the different units 
-    per = df[["Player", "Team", "Season", "Unit", "PER"]]
-    per = pd.pivot_table(data = per, values = "PER", index = ["Team", "Season", "Unit"], aggfunc= np.sum).reset_index()
-
-    # PER summed of all units
-    per_123_units = per.groupby(["Team", "Season"]).sum()["PER"].reset_index()
-    per_123_units["PER_Rank_all"] = per_123_units.groupby("Season")["PER"].rank("dense", ascending = False)
-    per_123_units.rename({"PER": "PER_Sum_all"}, axis = 1, inplace = True)
-
-    # PER summed of first and second unit
-    per = per[per.Unit != 3]
-    per_12_units = per.groupby(["Team", "Season"]).sum()["PER"].reset_index()
-    per_12_units["PER_Rank_FU_SU"] = per_12_units.groupby("Season")["PER"].rank("dense", ascending = False)
-    per_12_units.rename({"PER": "PER_Sum_FU_SU"}, axis = 1, inplace = True)
-
-    # PER summed of first unit
-    per = per[per.Unit != 2]
-    per_1_units = per.groupby(["Team", "Season"]).sum()["PER"].reset_index()
-    per_1_units["PER_Rank_FU"] = per_1_units.groupby("Season")["PER"].rank("dense", ascending = False)
-    per_1_units.rename({"PER": "PER_Sum_FU"}, axis = 1, inplace = True)
-
-    # Merge all dataframes 
-
-    PER_Rank = per_1_units.merge(per_12_units).merge(per_123_units)
-    
-    return PER_Rank
+def sum_stat_by_unit(player_stats, unit, stat):
+    return player_stats[player_stats.Unit == unit].groupby(["Team", "Season"]).sum()[stat]
